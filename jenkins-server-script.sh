c39e8335757d92d290fbf92e3e6e46edab2afa79
Shawnzy <mailo.shawn@gmail.com>
2023-12-13 09:15:52 -0800

    Deleted unnecessary blank lines in jenkins-server-script.sh

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -19,13 +19,11 @@ sudo systemctl start jenkins
 sudo yum install git -y
 
 # install terraform
-
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
 # install kubectl
-
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
